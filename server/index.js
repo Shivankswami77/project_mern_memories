9ccb07db077bdae930cc3beecae714699b3a6126
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-03-06 20:06:26 +0530

    Ui changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://ShivankSwami4712:Metro@123@mernapp.h3lrkvg.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://shivankdocuments77:W0fiVmOzlatwpyuT@cluster0.y2huv46.mongodb.net/";
 const PORT = process.env.PORT || 5000;
 
 mongoose
